6480f1558b1085eda5c226abee607a8ead3db614
khj6165 <khj6165@gmail.com>
2020-09-29 17:59:19 +0900

    base분리

--- lohbs_pick/settings.py
+++ lohbs_pick/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'lohbs_pick.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'lohbs_pick', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
