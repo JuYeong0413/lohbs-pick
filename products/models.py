c7fb85b2eb14db27e96d01007730a66cd94986fd
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-10-07 19:40:36 +0900

    product, collection, collectionProduct 모델 추가

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Product(models.Model):
+
+    PRODUCT_KIND_CHOICES = [
+        (0, "스킨케어"),
+        (1, "메이크업"),
+        (2, "헤어케어"),
+        (3, "바디케어"),
+        (4, "향수/방향"),
+        (5, "리빙/잡화"),
+        (6, "맨케어"),
+        (7, "헬스"),
+        (8, "푸드"),
+        (9, "베이비케어"),
+        (10, "펫케어"),
+    ]   
+
+    brand = models.CharField(_('브랜드명'), max_length=100) 
+    name = models.CharField(_('제품명'),max_length=100)
+    price = models.PositiveIntegerField(_('제품가격'))
+    image = models.ImageField(_('제품이미지'),upload_to='product_images/')
+    category = models.PositiveSmallIntegerField(_('제품종류'), choices = PRODUCT_KIND_CHOICES)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from products.models import Product
 
-# Create your models here.
+class CollectionProduct(models.Model):
+    product = models.OneToOneField(Product, verbose_name=_('제품'), blank=True, on_delete=models.CASCADE)
+    qunatity = models.PositiveSmallIntegerField(_('수량'))
+    sub_total = models.PositiveIntegerField(_('가격'))
+
+    def save(self, *args, **kwargs):
+        self.price = self.product.price*self.amount
+        super().save(*args, **kwargs)
+
+class Collection(models.Model):
+    collection_products = models.ManyToManyField(CollectionProduct, related_name="collection_products", symmetrical=False, blank=True, through='Collecting')
+    collection_price = models.PositiveIntegerField(_('총 가격'), blank=True)
+    period = models.PositiveSmallIntegerField(_('주기'))
+    name = models.CharField(_('컬렉션명'), max_length=100)
+
+    class Meta:
+        verbose_name='컬렉션'
+        verbose_name_plural='컬렉션'
+
+
+
+class Collecting(models.Model):
+    collection = models.ForeignKey(Collection, verbose_name= _('컬렉션'), on_delete=models.CASCADE)
+    collection_product = models.ForeignKey(CollectionProduct, verbose_name= _('컬렉션 제품'), on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name='컬렉팅'
+        verbose_name_plural='컬렉팅'
+        unique_together=(
+            ('collection', 'collection_product')
+        )
