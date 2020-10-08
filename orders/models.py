99ad8b1bab6c1c854227e54edbade457b1e1722a
Juyeong Lee <2jy22@naver.com>
2020-10-08 10:42:59 +0900

    컬렉션명

--- orders/models.py
+++ orders/models.py
@@ -8,7 +8,7 @@ class Order(models.Model):
     order_total = models.PositiveIntegerField() # 가격합계
     address = models.CharField(max_length=300) # 주소
     collection = models.ForeignKey(Collection, on_delete=models.SET_NULL) # 컬렉션
-    collection_name = models.CharField(max_length=50) # 컬렉션 이름
+    collection_name = models.CharField(max_length=100) # 컬렉션명
     order_products = models.ManyToManyField(OrderProduct, related_name="order_products", through="OrderProduct") # 주문상품
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -36,6 +36,7 @@ class Share(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL) # 작성자(사용자)
     image = models.ImageField(upload_to='s_images/') # 사진
     content = models.TextField() # 내용
+    collection_name = models.CharField(max_length=100) # 컬렉션명
     collection_products = models.ManyToManyField(Item, related_name="collection_products", through="CollectionProduct") # 컬렉션상품
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
