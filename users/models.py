d08d9ddbea0b1dddcccdd239a3a7641f33865b6e
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-10-08 11:59:36 +0900

    collection에 user 추가 및 수정

--- users/models.py
+++ users/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from products.models import Product
+from django.utils.models import User
+
 
 class CollectionProduct(models.Model):
-    product = models.OneToOneField(Product, verbose_name=_('제품'), blank=True, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, verbose_name=_('제품'), on_delete=models.CASCADE)
     qunatity = models.PositiveSmallIntegerField(_('수량'))
-    sub_total = models.PositiveIntegerField(_('가격'))
+    sub_total = models.PositiveIntegerField(_('CollectionProduct 가격'))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        self.price = self.product.price*self.amount
+        self.sub_total = self.product.price*self.qunatity
         super().save(*args, **kwargs)
 
 class Collection(models.Model):
@@ -23,9 +25,10 @@ class Collection(models.Model):
         ("3M", "3달"),
     ]   
 
+    user = models.ForeignKey(User, verbose_name=_('사용자'), on_delete=models.CASCADE)
     collection_products = models.ManyToManyField(CollectionProduct, related_name="collection_products", blank=True, through='Collecting')
-    collection_total = models.PositiveIntegerField(_('총 가격'), blank=True)
-    period = models.CharField(_('주기'), max_length=2, choices=PERIOD_CHOICES)
+    collection_total = models.PositiveIntegerField(_('Collection 가격'), default=0)
+    period = models.CharField(_('배송주기'), max_length=2, choices=PERIOD_CHOICES)
     name = models.CharField(_('컬렉션명'), max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -41,8 +44,6 @@ class Collecting(models.Model):
     collection_product = models.ForeignKey(CollectionProduct, verbose_name= _('컬렉션 제품'), on_delete=models.CASCADE)
     
     class Meta:
-        verbose_name='컬렉팅'
-        verbose_name_plural='컬렉팅'
         unique_together=(
             ('collection', 'collection_product')
         )
