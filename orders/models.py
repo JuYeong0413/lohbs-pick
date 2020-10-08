8390702944a03b085e3671123e580a41df31ba6d
Juyeong Lee <2jy22@naver.com>
2020-10-08 10:29:21 +0900

    모델링, 주석 수정

--- orders/models.py
+++ orders/models.py
@@ -9,13 +9,13 @@ class Order(models.Model):
     address = models.CharField(max_length=300) # 주소
     collection = models.ForeignKey(Collection, on_delete=models.SET_NULL) # 컬렉션
     collection_name = models.CharField(max_length=50) # 컬렉션 이름
-    collection_products = models.ManyToManyField(CollectionProduct, related_name="collection_products", through="CollectionProduct") # 컬렉션상품
+    order_products = models.ManyToManyField(OrderProduct, related_name="order_products", through="OrderProduct") # 주문상품
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class OrderProduct(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.SET_NULL) # 제품
+    product = models.OneToOneField(Product, on_delete=models.SET_NULL) # 상품
     quantity = models.PositiveIntegerField() # 수량
     price = models.PositiveIntegerField() # 가격
     created_at = models.DateTimeField(auto_now_add=True)
@@ -24,7 +24,7 @@ class OrderProduct(models.Model):
 
 class Ordering(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE) # 주문
-    order_products = models.ForeignKey(OrderProduct, on_delete=models.CASCADE) # 주문제품
+    order_products = models.ForeignKey(OrderProduct, on_delete=models.CASCADE) # 주문상품
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -34,7 +34,7 @@ class Ordering(models.Model):
 
 class Share(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL) # 작성자(사용자)
-    image = models.ImageField(upload_to='s_images/') # 이미지
+    image = models.ImageField(upload_to='s_images/') # 사진
     content = models.TextField() # 내용
     collection_products = models.ManyToManyField(Item, related_name="collection_products", through="CollectionProduct") # 컬렉션상품
     created_at = models.DateTimeField(auto_now_add=True)
