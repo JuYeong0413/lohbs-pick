a28433446eb38f88174341fc9993bbfef578c1a9
김효정 <123gywjddl@naver.com>
2020-10-12 07:42:35 +0000

    add mypage

--- lohbs_pick/urls.py
+++ lohbs_pick/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main),
     path('accounts/', include('allauth.urls')),
+    path('users/', include('users.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
 
-# Create your views here.
+
+# 프로필 페이지
+def main(request, id):
+    user_profile = get_object_or_404(User, pk=id)
+    return render(request, 'users/main.html', {'user_profile': user_profile})
+
