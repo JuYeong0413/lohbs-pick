f611b07a1bfb2ae7a3fef0bc723d8ae7a005d079
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-10-14 18:11:20 +0900

    카테고리 별 정리

--- products/views.py
+++ products/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Product
+import pdb
 
-def products_list(request):
+def main(request):
     products = Product.objects.all()
-    return render(request, 'products/products_list.html', {'products':products})
+    return render(request, 'products/main.html', {'products':products})
 
 def search(request):
     query = request.GET.get('query')
     search_products = Product.objects.filter(name__contains=query)
-    return render(request, 'products/search.html', {'search_products':search_products})
+    return render(request, 'products/search.html', {'search_products':search_products, 'query':query})
+
+def category_list(request):
+    category = request.GET.get('category')
+    category = int(category)
+    category_lists = Product.objects.filter(category=category)
+    return render(request, 'products/category_list.html', {'category_lists':category_lists, 'output':category})
