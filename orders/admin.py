17193105028f52456980a6e13720b7d0202b5fa6
Juyeong Lee <2jy22@naver.com>
2020-10-19 05:25:27 +0900

    orders, picks admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'collection',
+        'collection_name',
+        'created_at',
+    )
+
+    search_fields = (
+        'collection_name',
+    )
+
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'product',
+        'quantity',
+        'price',
+        'created_at',
+    )
