c80dcfd5b6a025779dd7fca1cd241485f29afa53
Juyeong Lee <2jy22@naver.com>
2020-10-19 03:27:57 +0900

    롭스픽 공유하기 사진 선택 시 파일명 및 미리보기 표시

---
+++ lohbs_pick/static/js/share.js
@@ -0,0 +1,24 @@
+function filePreview(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        let fileType = input.files[0].type;
+        let imageElement = document.getElementById('show-image-tag')
+
+        reader.onload = function (e) {
+            e.preventDefault()
+
+            if (fileType.includes('image') === true) {
+                if (imageElement !== null) { imageElement.remove(); }
+
+                let newImage = document.createElement('img'); 
+                $(newImage).attr('id', 'show-image-tag');
+                $(newImage).attr('src', e.target.result);
+                $(newImage).css('width', '50%');
+                
+                $('#file-content').append(newImage);
+                $('#file-content').addClass('mb-2');
+            }
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
