6d7e76eb61ad4847d7e6a61ffe950f8b711724e1
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-10-25 04:43:29 +0900

    메인 페이지 변경

--- lohbs_pick/views.py
+++ lohbs_pick/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from products.models import Product
+from picks.models import *
 
 def main(request):
     products = Product.objects.all()[0:8]
-    return render(request, 'index.html', {"products":products})
+    shared_lists = Share.objects.all().order_by('-created_at')[0:3]
+    return render(request, 'index.html', {"products":products, 'shared_lists':shared_lists})
