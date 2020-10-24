2ff37a913005467677449c699f6290783616605a
Juyeong Lee <2jy22@naver.com>
2020-10-24 10:25:08 +0900

    share delete Modal

--- lohbs_pick/static/js/share.js
+++ lohbs_pick/static/js/share.js
@@ -23,20 +23,3 @@ function filePreview(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
-
-// share main page
-function readMore() {
-  var dots = document.getElementById('dots');
-  var moreText = document.getElementById('more');
-  var btnText = document.getElementById('readMoreBtn');
-
-  if (dots.style.display === 'none') {
-    dots.style.display = 'inline';
-    btnText.innerHTML = 'Read More  <i class="fa fa-angle-right">';
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = 'none';
-    btnText.innerHTML = 'Read less';
-    moreText.style.display = 'inline';
-  }
-}
