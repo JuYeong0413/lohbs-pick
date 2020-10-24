d377ab1ba20dc8ac637639c415275ec2c6ddfc21
Juyeong Lee <2jy22@naver.com>
2020-10-25 02:21:27 +0900

    migration

--- orders/models.py
+++ orders/models.py
@@ -21,6 +21,15 @@ class OrderProduct(models.Model):
 
 
 class Order(models.Model):
+    PERIOD_CHOICES = [
+        ("1W", "1주"),
+        ("2W", "2주"),
+        ("3W", "3주"),
+        ("1M", "1달"),
+        ("2M", "2달"),
+        ("3M", "3달"),
+    ]
+
     user = models.ForeignKey(User, verbose_name=_('사용자'), on_delete=models.CASCADE)
     order_total = models.PositiveIntegerField(_('총 가격'))
     address = models.CharField(_('배송지 주소'), max_length=300)                    
@@ -30,6 +39,8 @@ class Order(models.Model):
     recipient = models.CharField(_('받는 사람'), max_length=10)
     recipient_phone = models.CharField(_('수령인 연락처'), max_length=15)
     delivery_message = models.TextField(_('배송 요청사항'), blank=True)
+    period = models.CharField(_('주기'), max_length=2, choices=PERIOD_CHOICES)
+    isValid = models.BooleanField(_('유효성'), default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
