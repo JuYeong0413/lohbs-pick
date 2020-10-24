50c2e6c584c982067626a323c059a5cf104a95de
ygk313 <50168972+ygk313@users.noreply.github.com>
2020-10-24 19:58:46 +0900

    main 페이지 글귀 변경, 상품 추가 및 footer 사진 추가

--- lohbs_pick/views.py
+++ lohbs_pick/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from products.models import Product
 
 def main(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()[0:8]
+    return render(request, 'index.html', {"products":products})
