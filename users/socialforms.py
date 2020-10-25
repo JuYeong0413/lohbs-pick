17e0780506529bfc938f0f37ae9d958ebd99edb1
Juyeong Lee <2jy22@naver.com>
2020-10-25 21:49:11 +0900

    로그인, 회원가입 페이지 개선

--- users/socialforms.py
+++ users/socialforms.py
@@ -2,7 +2,7 @@ from allauth.socialaccount.forms import SignupForm
 from django import forms
 
 class MyCustomSocialSignupForm(SignupForm):
-    nickname = forms.CharField(label='닉네임')
+    nickname = forms.CharField(label='닉네임', widget=forms.TextInput(attrs={'placeholder': '닉네임'}))
 
     def save(self, request):
         user = super(MyCustomSocialSignupForm, self).save(request)
